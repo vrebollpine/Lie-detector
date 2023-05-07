21113ed2f6a3f7dc078f5f193069fd24409365b3
vrebollpine <126656622+vrebollpine@users.noreply.github.com>
2023-05-07 16:29:58 +0200

    Update lie_detector.ino

--- lie_detector.ino
+++ lie_detector.ino
@@ -18,35 +18,34 @@ int co2Pin = A0;
 Adafruit_AHTX0 aht;
 int average_hr;
 int hr_sum;
-int num_pulsaciones;
+int num_loop;
 
 int average_co2;
 int co2_sum;
-int num_co2;
 
 int num_anomalies;
 
 int average_humd;
-int num_humd;
 int humd_sum;
 
 void setup() {
 // initialize serial communication at 9600 bits per second:
   Serial.begin(115200);
+
   if (! aht.begin()) {
   Serial.println("Could not find AHT? Check wiring");
   while (1) delay(10);
   }
   Serial.println("AHT10 or AHT20 found");
 
-  pinMode(3, OUTPUT); //red led light
+  pinMode(3, OUTPUT); //green led light
   pinMode(4, OUTPUT); //yellow led light
-  pinMode(5, OUTPUT); //green led light
+  pinMode(5, OUTPUT); //red led light
 }
 void loopFalse(){
-  digitalWrite(3, HIGH);
+  digitalWrite(3, LOW);
   digitalWrite(4, LOW);
-  digitalWrite(5, LOW); 
+  digitalWrite(5, HIGH); 
 }
 
 void loopUncertain(){
@@ -56,9 +55,9 @@ void loopUncertain(){
 }
 
 void loopTrue(){
-  digitalWrite(3, LOW);
+  digitalWrite(3, HIGH);
   digitalWrite(4, LOW);
-  digitalWrite(5, HIGH);
+  digitalWrite(5, LOW);
 }
 void loop2(){
   if (num_anomalies == 0){
@@ -77,51 +76,48 @@ void loop() {
 
   int heartRate = analogRead(A1);
   heartRate = map(heartRate, 0, 1500, 0, 200);
-  Serial.println(heartRate);
+  Serial.print("Heart rate: ");
+  Serial.print(heartRate);
 
   int co2Value = analogRead(co2Pin); // Read the sensor value
   float voltage = co2Value * (5.0 / 1023.0); // Convert the sensor value to voltage
   float ratio = voltage / 4.9; // Calculate the ratio of the sensor voltage to the reference voltage (4.9V)
   float concentration = (1.67 * pow(ratio, -1.23)) * 1000; // Calculate the concentration of CO2 in ppm
-
-  Serial.print("CO2 concentration: ");
+  Serial.print(" CO2 concentration: ");
   Serial.print(concentration);
-  //Serial.println(" ppm");
 
   sensors_event_t humidity, temp;
   aht.getEvent(&humidity, &temp);// populate temp and humidity objects with fresh data
-  //Serial.print("Temperature: "); Serial.print(temp.temperature); Serial.println("degrees C");
-  Serial.print("Humidity: "); Serial.print(humidity.relative_humidity);
+  int humd = humidity.relative_humidity;
+  Serial.print(" Humidity: "); 
+  Serial.print(humidity.relative_humidity);
   Serial.println("% rH");
 
+  num_loop ++;
   hr_sum += heartRate;
-  num_pulsaciones += 1;
-  average_hr = hr_sum / num_pulsaciones;
+  average_hr = hr_sum /num_loop;
 
   co2_sum += concentration;
-  num_co2 += 1;
-  average_co2 = co2_sum / num_co2;
+  average_co2 = co2_sum /num_loop;
 
   humd_sum += humidity.relative_humidity;
-  num_humd ++;
-  average_humd = humd_sum/num_humd;
+  average_humd = humd_sum/num_loop;
   
   
-
-  if (heartRate > average_hr + 10 || heartRate < average_hr-10) {
-    num_anomalies++;
+  if (heartRate > average_hr + 30 || heartRate < average_hr-30) {
+    num_anomalies++; 
   }
   
-  if (concentration > average_co2 +  200|| concentration < average_co2 -200) {
+  if (concentration > average_co2 +  10000|| concentration < average_co2 -10000) {
 	  num_anomalies ++;
   }
 
   if (humidity.relative_humidity > average_humd + 3 || humidity.relative_humidity < average_humd -3){
-    num_anomalies ++;
+    num_anomalies ++; 
   }  
-  Serial.println(num_anomalies);
-
+  
   loop2();
   
   delay(1000); // delay in between reads for stability
 }
+
